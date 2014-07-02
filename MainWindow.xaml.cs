fd2c9a4b381202772a60d12985bc5f169ab05411
Drio <Drio@localhost>
2014-07-02 10:22:04 +0200

    creation

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.IO;
+using Ookii.Dialogs.Wpf;
+
+namespace Resizer
+{
+    public partial class MainWindow : Window
+    {
+        private static int num_file_done;
+        private static int num_files;
+        private ResizerCore resizer;
+        private VistaFolderBrowserDialog dialog;
+        private Thread thread;
+
+
+        private void resizer_done(string file)
+        {
+            num_file_done++;
+            Dispatcher.Invoke(new Action(() => { 
+                lbl_progress.Content = num_file_done.ToString() + "/" + num_files.ToString();
+            }));
+        }
+
+        private void resizer_finished()
+        {
+            Dispatcher.Invoke(new Action(() =>
+            {
+                bt_resize.IsEnabled = true;
+                bt_stop.IsEnabled = false;
+            }));
+            MessageBox.Show("Resize done", "Mazeltof", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            dialog = new VistaFolderBrowserDialog();
+
+            num_file_done = 0;
+
+            resizer = new ResizerCore();
+            resizer.file_done += resizer_done;
+            resizer.finished  += resizer_finished; 
+
+            slider.ValueChanged += slider_ValueChanged;
+        }
+
+        private void bt_src_Click(object sender, RoutedEventArgs e)
+        {
+            dialog.Description = "Please select a folder.";
+            dialog.UseDescriptionForTitle = true;
+            if ((bool)dialog.ShowDialog(this))
+            {
+                tb_src.Text = dialog.SelectedPath;
+            }
+        }
+
+        private void bt_dest_Click(object sender, RoutedEventArgs e)
+        {
+            dialog.Description = "Please select a folder.";
+            dialog.UseDescriptionForTitle = true;
+            if ((bool)dialog.ShowDialog(this))
+            {
+                tb_dest.Text = dialog.SelectedPath;
+            }
+        }
+
+        private void bt_resize_Click(object sender, RoutedEventArgs e)
+        {
+            //Src checks
+            if (string.IsNullOrWhiteSpace(tb_src.Text))
+            {
+                MessageBox.Show("Source must not be empty", "Wrong path", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+            if (!Directory.Exists(tb_src.Text))
+            {
+                MessageBox.Show("Source directory does not exist", "Wrong path", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            //Dest checks
+            if (string.IsNullOrWhiteSpace(tb_dest.Text))
+            {
+                MessageBox.Show("Destination must not be empty", "Wrong path", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+            if (!Directory.Exists(tb_dest.Text))
+            {
+                MessageBox.Show("Destination directory does not exist", "Wrong path", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            // path comparaisons
+            if (cb_recursive.IsChecked == true)
+            {
+                if (tb_dest.Text == tb_src.Text || tb_dest.Text.Contains(tb_src.Text))
+                {
+                    MessageBox.Show("The destination path must not be include in source path", "Wrong path", MessageBoxButton.OK, MessageBoxImage.Warning);
+                    return;
+                }
+            }
+            else
+            {
+                if (tb_dest.Text == tb_src.Text)
+                {
+                    MessageBox.Show("The source path must be different from destination path", "Wrong path", MessageBoxButton.OK, MessageBoxImage.Warning);
+                    return;
+                }
+            }
+
+
+            ResizerContext data = new ResizerContext(Directory.GetFiles(tb_src.Text, "*.JPG"), tb_dest.Text, tb_dest.Text, (int)slider.Value);
+
+            num_file_done = 0;
+            num_files = data.files.Length;
+
+            set_ui_state(false);
+
+            thread = new Thread(() => resizer.run(data));
+            thread.Start();
+
+        }
+
+        void set_ui_state(bool value)
+        {
+            bt_resize.IsEnabled = value;
+            slider.IsEnabled = value;
+            bt_stop.IsEnabled = !value;
+            pgrogress_bar.Visibility = value ? System.Windows.Visibility.Hidden : System.Windows.Visibility.Visible;
+        }
+        
+        private void bt_stop_Click(object sender, RoutedEventArgs e)
+        {
+            set_ui_state(true);
+            
+            lbl_progress.Content = string.Empty;
+
+            thread.Abort();
+        }
+
+        private void bt_close_Click(object sender, RoutedEventArgs e)
+        {
+            if(thread != null) thread.Abort();
+            this.Close();
+        }
+
+        private void slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            lbl_slider.Content = ((int)e.NewValue).ToString() + " %";
+        }
+
+    }
+}
---
+++ ResizerCore.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+
+namespace Resizer
+{
+    public delegate void resize_done(string file);
+    public delegate void resize_finished();
+    
+    class ResizerContext
+    {
+        public ResizerContext(string[] _files, string _src, string _dest, int _quality)
+        {
+            files = _files;
+            src = _src;
+            dest = _dest;
+            quality = _quality;
+        }
+        public string[] files;
+        public string src;
+        public string dest;
+        public int quality;
+
+    }
+
+    class ResizerCore
+    {
+        public event resize_done     file_done;
+        public event resize_finished finished;
+
+        private void OnFileDone(string data)
+        {
+            var handler = file_done;
+            if (handler != null)
+            {
+                handler(data);
+            }
+        }
+
+        private void OnFinished()
+        {
+            var handler = finished;
+            if (handler != null)
+            {
+                handler();
+            }
+        }
+
+        public void run(ResizerContext ctx)
+        {
+            Stream BitmapStream;
+            System.Drawing.Image img;
+            Bitmap image;
+
+            foreach (string file in ctx.files)
+            {
+                using (BitmapStream = System.IO.File.Open(file, System.IO.FileMode.Open))
+                {
+                    using (img = System.Drawing.Image.FromStream(BitmapStream))
+                    {
+                        using (image = new Bitmap(img))
+                        {
+                            string filedest = System.IO.Path.Combine(ctx.dest, System.IO.Path.GetFileName(file));
+
+                            if (image.Width != 3648 && image.Width != 2736) continue;
+
+                            if (image.Width == 3648)
+                            {
+                                resize(image, filedest, 1280, 960, ctx.quality);
+                            }
+                            else
+                            {
+                                resize(image, filedest, 960, 1280, ctx.quality);
+                            }
+                        }
+                    }
+                }
+                OnFileDone(file);
+            }
+            OnFinished();
+        }
+
+        private static ImageCodecInfo GetEncoderInfo(ImageFormat format)
+        {
+            return ImageCodecInfo.GetImageDecoders().SingleOrDefault(c => c.FormatID == format.Guid);
+        }
+
+        private static void resize(Bitmap image, string dest, int maxWidth, int maxHeight, int quality)
+        {
+            // Get the image's original width and height
+            int originalWidth = image.Width;
+            int originalHeight = image.Height;
+
+            // To preserve the aspect ratio
+            float ratioX = (float)maxWidth / (float)originalWidth;
+            float ratioY = (float)maxHeight / (float)originalHeight;
+            float ratio = Math.Min(ratioX, ratioY);
+
+            // New width and height based on aspect ratio
+            int newWidth = (int)(originalWidth * ratio);
+            int newHeight = (int)(originalHeight * ratio);
+
+            // Convert other formats (including CMYK) to RGB.
+            using (Bitmap newImage = new Bitmap(newWidth, newHeight, System.Drawing.Imaging.PixelFormat.Format24bppRgb))
+            {
+                // Draws the image in the specified size with quality mode set to HighQuality
+                using (Graphics graphics = Graphics.FromImage(newImage))
+                {
+                    graphics.CompositingQuality = CompositingQuality.HighQuality;
+                    graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                    graphics.SmoothingMode = SmoothingMode.HighQuality;
+                    graphics.DrawImage(image, 0, 0, newWidth, newHeight);
+                }
+
+                // Get an ImageCodecInfo object that represents the JPEG codec.
+                ImageCodecInfo imageCodecInfo = GetEncoderInfo(ImageFormat.Jpeg);
+
+                // Create an Encoder object for the Quality parameter.
+                System.Drawing.Imaging.Encoder encoder = System.Drawing.Imaging.Encoder.Quality;
+
+                // Create an EncoderParameters object. 
+                EncoderParameters encoderParameters = new EncoderParameters(1);
+
+                // Save the image as a JPEG file with quality level.
+                EncoderParameter encoderParameter = new EncoderParameter(encoder, quality);
+                encoderParameters.Param[0] = encoderParameter;
+
+                newImage.Save(dest, imageCodecInfo, encoderParameters);
+            }
+        }
+    }
+}
